c4379aaf681eef0ad953cab3e0d4db02373aa50b
Cane <ifullstackweb@gmail.com>
2018-07-10 07:45:33 +0200

    Google Sign In in corporado

---
+++ README.md
@@ -0,0 +1,2 @@
+#Backend-Server
+Este es el código necesario para establecer una conexión a Mongodb usando Moongose
--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '518186354714-hpqd9m4ig2g5csdg953aq8141n49ntom.apps.googleusercontent.com';
